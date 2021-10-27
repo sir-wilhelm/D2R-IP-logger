ef995e811f738a33cbceabb18d579e7ffc1f89d9
sir-wilhelm <sir-wilhelm@users.noreply.github.com>
2021-10-26 22:30:03 -0500

    Formatting.

--- D2R_ip.py
+++ D2R_ip.py
@@ -10,9 +10,9 @@ update_interval = 1     # refresh rate in seconds; setting lower than 1 might be
 logfile = 'D2R_ip_{}.log'.format(strftime('%Y-%m-%d'))
 
 logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s, %(message)s', datefmt='%Y-%m-%d, %H:%M:%S',
-    handlers=[
+    level = logging.INFO,
+    format = '%(asctime)s, %(message)s', datefmt='%Y-%m-%d, %H:%M:%S',
+    handlers = [
         logging.FileHandler(logfile),   # remove this line to stop logging to a file
         logging.StreamHandler()
     ]
@@ -48,7 +48,7 @@ previous_time = 0
 hunting_ip = ''
 hunting_ip_found = False
 
-if (len(sys.argv) >= 2):
+if len(sys.argv) >= 2:
     print('Hunting for ip: ' + Fore.LIGHTRED_EX + sys.argv[1] + Style.RESET_ALL)
     hunting_ip = sys.argv[1]
 
@@ -71,30 +71,30 @@ def print_ip():
     global hunting_ip_found
     region = '?'
     for c in p.connections('tcp'):
-        if (c.raddr):
+        if c.raddr:
             ip = c.raddr.ip
-            if(ip in EU_client):
+            if ip in EU_client:
                 region = 'EU'
-            elif(ip in NA_client):
+            elif ip in NA_client:
                 region = 'NA'
-            elif(ip in Asia_client):
+            elif ip in Asia_client:
                 region = 'Asia'
-            elif(ip not in global_client):
+            elif ip not in global_client:
                 current_game_ip = ip
-    if(current_game_ip!=previous_ip):
+    if current_game_ip != previous_ip:
         # found a new game ip, log it
         print(' '*44, end="\r", flush=True) #clear line
         logging.info('{}, {}'.format(region, current_game_ip))
         previous_ip = current_game_ip
         previous_time = time()
         hunting_ip_found = False
-    if(current_game_ip == hunting_ip and not hunting_ip_found):
+    if current_game_ip == hunting_ip and not hunting_ip_found:
         print(Fore.LIGHTRED_EX + 'Clone IP found!' + Style.RESET_ALL)
         hunting_ip_found = True
     # display clock, coloured green if more then 1 minute passed since last game creation:
     msg = datetime.now().strftime('%Y-%m-%d, %H:%M:%S')
     info = ' (press CTRL+C to exit)'
-    if previous_time>0 and time()-previous_time>=60:
+    if previous_time > 0 and time()-previous_time >= 60:
         print(Fore.GREEN + msg + Style.RESET_ALL + info, end="\r", flush=True)
     else:
         print(msg + info, end="\r", flush=True)
